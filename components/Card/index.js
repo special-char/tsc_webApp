83ab2e801cec0cd47441bfcb0673fa702f9a4ba9
Yagnesh Modh <yagnesh.modh@gmail.com>
2022-07-25 11:59:24 +0530

    commit code

--- components/Card/index.js
+++ components/Card/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import cn from "classnames";
-import Button from "./Button";
-import Image from "./Image";
+import Button from "../Button";
+import Image from "../Image";
 
 const Card = ({
   svg,
