bfcbfff021d32f0f3bcc21775ca3752e92e757f0
Hemang <hemangmesariya009@gmail.com>
2022-07-27 12:00:33 +0530

    homeSection CourseBycategory CourseSection ResourcesSection EducationCourses added

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,10 @@ import Svg from "@public/icons/kidOnLaptop.svg";
 import Svg1 from "@public/icons/graduate.svg";
 import Svg2 from "@public/icons/studying.svg";
 import { useEffect } from "react";
+import CourseByCategory from "@components/PageSections/courseByCategory";
+import ResourcesSection from "@components/PageSections/resourcesSection";
+import EducationCourses from "@components/PageSections/educationCourses";
+
 export default function Home() {
   return (
     <div>
@@ -57,6 +61,19 @@ export default function Home() {
         <div className="avatar">
           <div className="w-14">
             <img src="https://placeimg.com/192/192/people" />
+          </div>
+        </div>
+        <div className="flex">
+        <div className="avatar">
+          <div className="w-48">
+            <img src="https://placeimg.com/192/192/people" />
+          </div>
+          </div>  
+          <div className="px-5 py-5">
+          <p>
+            “Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint”
+          </p>
           </div>
         </div>
 
@@ -119,6 +136,7 @@ export default function Home() {
                   </div>
                 </div>
                 <div className="author__name">Yagnesh Modh</div>
+<<<<<<< HEAD
               </div>
             </div>
             <input
@@ -174,10 +192,13 @@ export default function Home() {
                   </div>
                   <div className="author__name">Yagnesh Modh</div>
                 </div>
+=======
+>>>>>>> dbd4695 (homeSection CourseBycategory CourseSection ResourcesSection EducationCourses added)
               </div>
             </div>
           </div>
 
+<<<<<<< HEAD
           <div className="card w-1/3">
             <figure>
               <img
@@ -249,9 +270,31 @@ export default function Home() {
                 </div>
               </div>
             </div>
+=======
+        <div className="card w-1/3">
+          <figure>
+            <img
+              src="https://assets.website-files.com/607de2d8e8911e32707a3efe/607f500fe9f6c983c68e88ef_image-1-courses-categories-education-x-template.svg"
+              alt="Shoes"
+            />
+          </figure>
+          <div className="card-body">
+            <h3 className="card-title">Graphic Design 101</h3>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Reiciendis, nobis libero labore temporibus mollitia neque est
+              adipisci enim delectus fugit voluptate unde quisquam perspiciatis
+              earum, minus cum dolore molestias error!
+            </p>
+>>>>>>> dbd4695 (homeSection CourseBycategory CourseSection ResourcesSection EducationCourses added)
           </div>
         </div>
       </div>
+      <HomeSection />
+      <CourseByCategory />
+      <CourseSection />
+      <ResourcesSection />
+      <EducationCourses />
     </div>
   );
 }
