ff235ec1ec3b5022ae456228bb5d9e90e1421257
PratikN7572 <subwaysurfersnewyork@gmail.com>
2022-07-27 14:45:19 +0530

    latest change commit

--- components/PageSections/educationCourses/index.js
+++ components/PageSections/educationCourses/index.js
@@ -26,11 +26,20 @@ const EducationCourses = () => {
               <p>Happy students worldwide</p>
             </div>
           </div>
+          <div className="home-wrapper flex">
+            <div className="container items-center justify-center text-center">
+              <h2>Grow your career today with the Education courses</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt.
+              </p>
+              <div className="button-wrapper">
+                <button className="btn btn--secondary">Explore Courses</button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
   );
 };
-
-EducationCourses.displayName = "EducationCourses";
-export default EducationCourses;
