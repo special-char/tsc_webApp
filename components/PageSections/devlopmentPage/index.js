3a6f47b9c13e8f95ceb3810bd52bdfe76f64f485
Devanshi73 <contact.devanshi.desai@gmail.com>
2022-09-07 17:20:48 +0530

    Layout change

--- components/PageSections/devlopmentPage/index.js
+++ components/PageSections/devlopmentPage/index.js
@@ -1,14 +1,9 @@
-import Footer from "@components/Footer";
 import HeaderSection from "@components/PageSections/devlopmentPage/headerSection";
-import OurDevlopers from "@components/PageSections/devlopmentPage/ourDevlopers";
 import OurProjectsSection from "@components/PageSections/devlopmentPage/ourProjects";
 import OurServices from "@components/PageSections/devlopmentPage/ourServices";
 import TestimonialSection from "@components/PageSections/devlopmentPage/testimonialSection";
-import React from "react";
 import CareerForm from "./careerForm";
 import ContactUS from "./contactUs";
-import HeaderSection from "./headerSection";
-import OurDevlopers from "./ourDevlopers";
 import OurProjectsSection from "./ourProjects";
 import OurTeam from "./ourTeam";
 
@@ -16,14 +11,12 @@ const DevelopmentPage = () => {
   return (
     <>
       <HeaderSection />
-      <OurTeam />
+      <OurServices />
       <OurProjectsSection />
-
+      <OurTeam />
+      <TestimonialSection />
       <CareerForm />
       <ContactUS />
-      <OurServices />
-      <TestimonialSection />
-      <Footer />
     </>
   );
 };
