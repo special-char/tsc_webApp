2b96c346d01b4fba1f1f11b6824d4c9985bfe1c4
dhara dave <imdavedhara@gmail.com>
2022-09-07 16:48:24 +0530

    Testimonial

--- pages/devlopment.js
+++ pages/devlopment.js
@@ -2,6 +2,7 @@ import Footer from "@components/Footer";
 import HeaderSection from "@components/PageSections/devlopmentPage/headerSection";
 import OurDevlopers from "@components/PageSections/devlopmentPage/ourDevlopers";
 import OurProjectsSection from "@components/PageSections/devlopmentPage/ourProjects";
+import TestimonialSection from "@components/PageSections/devlopmentPage/testimonialSection";
 import React from "react";
 
 const devlopment = () => {
@@ -10,6 +11,7 @@ const devlopment = () => {
       <HeaderSection />
       <OurDevlopers />
       <OurProjectsSection />
+      <TestimonialSection />
       <Footer />
     </>
   );
