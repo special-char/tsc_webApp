e289c8ca3e28b22edfd50b298877dcae55f45aaf
Devanshi73 <contact.devanshi.desai@gmail.com>
2022-09-08 11:35:24 +0530

    Banner

--- components/PageSections/devlopmentPage/headerSection/index.js
+++ components/PageSections/devlopmentPage/headerSection/index.js
@@ -6,7 +6,9 @@ const DevelopmentHeader = ({ data }) => {
       <section className="main flex items-center h-auto relative overflow-hidden py-16 lg:py-32">
         <div className="container flex flex-wrap items-center justify-center gap-16 max-w-7xl mx-auto px-4">
           <div className="text-content md:max-w-[535px] flex-1 flex flex-col justify-center text-center lg:text-left">
-            <h1 className="font-semibold">{data?.bannerDetails?.title}</h1>
+            <h1 className="font-semibold">
+              Grow your skills, define your future
+            </h1>
             <p className="mb-14">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum,
               tempora. Pariatur sint iure ipsum dignissimos similique,
@@ -16,7 +18,7 @@ const DevelopmentHeader = ({ data }) => {
           </div>
           <div className="relative basis-[500px] aspect-h-image h-auto rounded-3xl overflow-hidden skew-x-0 skew-y-0 scale-100 rotate-0 translate-x-1 translate-y-1 duration-1000 delay-1000 transition-all">
             <Image
-              src={data.bannerImage.name}
+              src="https://assets.website-files.com/607de2d8e8911e32707a3efe/607e15d7c0c9f4037bde6f44_image-home-hero-education-x-template-p-800.jpeg"
               layout={"fill"}
               objectFit={"cover"}
               objectPosition={"center"}
