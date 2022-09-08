88e58ab8f6fb859220a103ff8987e5fdc0a00418
Devanshi73 <contact.devanshi.desai@gmail.com>
2022-09-08 11:32:22 +0530

    Banner changes

--- components/PageSections/devlopmentPage/headerSection/index.js
+++ components/PageSections/devlopmentPage/headerSection/index.js
@@ -1,25 +1,22 @@
 import React from "react";
 import Image from "next/image";
-import Link from "next/link";
-import BannerImgJpeg from "/public/img/bannerimg.jpeg";
-
-const HeaderSection = () => {
+const DevelopmentHeader = ({ data }) => {
   return (
     <>
       <section className="main flex items-center h-auto relative overflow-hidden py-16 lg:py-32">
         <div className="container flex flex-wrap items-center justify-center gap-16 max-w-7xl mx-auto px-4">
           <div className="text-content md:max-w-[535px] flex-1 flex flex-col justify-center text-center lg:text-left">
-            <h1 className="font-semibold">
-              Grow your skills, define your future
-            </h1>
+            <h1 className="font-semibold">{data?.bannerDetails?.title}</h1>
             <p className="mb-14">
-              Presenting Academy, the tech school of the future. We teach you
-              the right skills to be prepared for tomorrow.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum,
+              tempora. Pariatur sint iure ipsum dignissimos similique,
+              recusandae non maiores? Praesentium placeat ipsum quibusdam. Id,
+              mollitia?
             </p>
           </div>
           <div className="relative basis-[500px] aspect-h-image h-auto rounded-3xl overflow-hidden skew-x-0 skew-y-0 scale-100 rotate-0 translate-x-1 translate-y-1 duration-1000 delay-1000 transition-all">
             <Image
-              src={BannerImgJpeg}
+              src={data.bannerImage.name}
               layout={"fill"}
               objectFit={"cover"}
               objectPosition={"center"}
@@ -33,5 +30,6 @@ const HeaderSection = () => {
     </>
   );
 };
-HeaderSection.displayName = "HeaderSection";
-export default HeaderSection;
+
+DevelopmentHeader.displayName = "DevelopmentHeader";
+export default DevelopmentHeader;
