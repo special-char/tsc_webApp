ee0f7c0c720394bc87f0fb4f6b990933e1af103f
Devanshi73 <contact.devanshi.desai@gmail.com>
2022-09-22 18:47:10 +0530

    Card width done

--- components/Card/index.js
+++ components/Card/index.js
@@ -4,7 +4,7 @@ import TSCImage from "@components/TSCImage";
 const Card = ({ ...obj }) => {
   return (
     <>
-      <div className="card">
+      <div className="card max-w-screen-xs sm:max-w-xl">
         <figure className="relative md:h-80">
           <TSCImage
             layout="fill"
