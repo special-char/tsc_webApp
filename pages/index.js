a1dbd27515cc6268d59374188f777b9ac73e8af5
dhara dave <imdavedhara@gmail.com>
2022-10-13 14:30:42 +0530

    landing page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
 import LandingPage from "@components/LandingPage";
 import React from "react";
-
+import { LayoutContext } from "context/layoutContext";
 const Home = () => {
-  return <LandingPage />;
+  return (
+    <LayoutContext.Consumer>
+      {(data) => {
+        return <LandingPage data={data} />;
+      }}
+    </LayoutContext.Consumer>
+  );
 };
 
 Home.displayName = "Home";
